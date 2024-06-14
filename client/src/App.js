ea73fd546421eb53e68603de7c89095fe9b3ee7e
PKashii <przemekkaczo@gmail.com>
2024-06-14 02:30:09 +0200

    minor changes - now tauri starts both server and client, also renamed the folders to better represent the structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,31 @@
+import React from "react";
 import {
   createBrowserRouter,
   createRoutesFromElements,
   RouterProvider,
   Route,
-  Link,
 } from "react-router-dom";
 
 import "./App.css";
-
+import RootLayout from "./layout/RootLayout";
 import Home from "./SubPage/Home";
 import ChampionShow from "./SubPage/ChampionShow";
 import Login from "./SubPage/Login";
-import RootLayout from "./layout/RootLayout";
 import Register from "./SubPage/Register";
 import Favorites from "./SubPage/Favorites";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="/build/:id" element={<ChampionShow />}></Route>
-      <Route path="/login" element={<Login />}></Route>
-      <Route path="/register" element={<Register />}></Route>
-      <Route path="/favorites" element={<Favorites />}></Route>
+      <Route path="/" element={<Home />} />
+      <Route path="/build/:id" element={<ChampionShow />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/favorites" element={<Favorites />} />
     </Route>
   )
 );
+
 const App = () => {
   return (
     <div>
